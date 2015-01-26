a738036c8d900c3ab031ac9a63e9203c1db32903
osdwebdev <osdwebdev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-26 20:00:03 +0000

    1.5 Release
    git-svn-id: https://plugins.svn.wordpress.org/osd-outdated-browser/tags/1.5@1076216 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ osd_outdated_browser_options.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Options for osd_outdated_browser
+ * ----------------------------------------------------------------------------
+ */
+
+if (is_admin()) {
+    add_action('admin_menu', 'osd_outdated_browser_options');
+    add_action('admin_init', 'osd_outdated_browser_register_options');
+    add_filter("plugin_action_links_osd-outdated-browser/osd_outdated_browser.php", "osd_outdated_browser_settings_link"); // Add settings link to plugins page
+}
+
+// Add submenu of options
+function osd_outdated_browser_options() {
+    add_submenu_page('options-general.php', 'OSD Outdated Browser', 'OSD Outdated Browser', 'manage_options', 'osd-outdated-browser-options', 'osd_outdated_browser_options_callback'); 
+}
+
+
+// Output options HTML
+function osd_outdated_browser_options_callback() {
+    ?>
+    <style>
+        #pre-div, #post-div { float: left; width: 50%; }
+        .cont:after { content: ""; display: block; clear: both; }
+        label { font-weight: 700; font-size: 1.1em; }
+        li { padding-top: 20px; }
+        .desc { margin-bottom: .5em; font-style: italic; }
+    </style>
+    <h2>OSD Outdated Browser</h2>
+    <p>Display a banner on outdated browsers informing users to update their browsers.</p>
+    <form action="options.php" method="post">
+        <?php 
+            settings_fields('osd_outdated_browser_options');
+            do_settings_sections('osd_outdated_browser_options');
+            $options = get_option('osd_outdated_browser_options');
+            $options['message'] = get_option('osd_outdated_browser_message');
+            global $osd_outdated_browser_instance;
+
+            foreach ($osd_outdated_browser_instance->defaults as $key => $value) {
+                if ($key == "message") {
+                    $value = str_replace("###URL###", plugins_url(), $osd_outdated_browser_instance->defaults['message']);
+                }
+                $options[$key] = ($options[$key]) ? $options[$key] : $value;
+            }
+        ?>
+        <ul>
+            <li>
+                <label for='osd_outdated_browser_options[browser]'>Browser Version Threshold:</label><br>
+                <div class='desc'>All versions equal to and below this value will display a banner</div>
+                <select name='osd_outdated_browser_options[browser]' value='<?php echo $options['browser']; ?>'>
+                    <option <?php selected($options['browser'], 'none'); ?> value=''>None</option>
+                    <option <?php selected($options['browser'], 'all'); ?> value='all'>All</option>
+                    <option <?php selected($options['browser'], '11'); ?> value='11'>IE 11</option>
+                    <option <?php selected($options['browser'], '10'); ?> value='10'>IE 10</option>
+                    <option <?php selected($options['browser'], '9'); ?> value='9'>IE 9</option>
+                    <option <?php selected($options['browser'], '8'); ?> value='8'>IE 8</option>
+                    <option <?php selected($options['browser'], '7'); ?> value='7'>IE 7</option>
+                    <option <?php selected($options['browser'], '6'); ?> value='6'>IE 6</option>
+                </select>
+            </li>
+            <li>
+                <label for='osd_outdated_browser_options[cookie]'>Cookie duration:</label><br>
+                <div class='desc'>This sets the length of time the cookie lasts when a user closes the outdated browser banner</div>
+                <select name='osd_outdated_browser_options[cookie]' value='<?php echo $options['cookie']; ?>'>
+                    <option <?php selected($options['cookie'], 'no-cookie'); ?> value='no-cookie'>Don't set a cookie</option>
+                    <option <?php selected($options['cookie'], 'session'); ?> value='session'>Session</option>
+                    <option <?php selected($options['cookie'], 'minutes'); ?> value='minutes'>30 Minutes</option>
+                    <option <?php selected($options['cookie'], 'hour'); ?> value='hour'>1 Hour</option>
+                    <option <?php selected($options['cookie'], 'day'); ?> value='day'>1 Day</option>
+                    <option <?php selected($options['cookie'], 'month'); ?> value='month'>1 Month</option>
+                    <option <?php selected($options['cookie'], 'year'); ?> value='year'>1 Year</option>
+                </select>
+            </li>
+            <li>
+                <label for="osd_outdated_browser_options[bg-color]">Background Color:</label>
+                <div class="desc">Background color of the banner</div>
+                <input type="text" id='bg-color' name='osd_outdated_browser_options[bg-color]' value='<?php echo $options['bg-color']; ?>' />
+            </li>
+            <li>
+                <label for="osd_outdated_browser_options[font-color]">Font Color:</label>
+                <div class="desc">Font color of the banner</div>
+                <input type="text" id='font-color' name='osd_outdated_browser_options[font-color]' value='<?php echo $options['font-color']; ?>' />
+            </li>
+            <li>
+                <label for='osd_outdated_browser_option_message'>Message:</label>
+                <div class="desc">Message that displays in the banner</div>
+                <?php wp_editor(apply_filters('content_edit_pre', $options['message']), 'content', array('textarea_name'=>'osd_outdated_browser_message')); ?>
+            </li>
+        </ul>
+        <?php submit_button(); ?>
+        <script>
+            jQuery(document).ready(function () {
+                jQuery('#bg-color').wpColorPicker();
+                jQuery('#font-color').wpColorPicker();
+            });
+        </script>
+    </form>
+    <?php
+}
+
+
+// Applies the filter for content_edit_pre before the field is saved
+function apply_content_edit_filter($content) {
+    return apply_filters('content_save_pre', $content);
+}
+
+
+// Register the options with WP
+function osd_outdated_browser_register_options() {
+    register_setting('osd_outdated_browser_options', 'osd_outdated_browser_options');
+    register_setting('osd_outdated_browser_options', 'osd_outdated_browser_message', 'apply_content_edit_filter');
+}
+
+
+// Show settings link in plugins screen
+function osd_outdated_browser_settings_link($links) {
+    $settings_link = "<a href='admin.php?page=osd-outdated-browser-options'>Settings</a>";
+    array_unshift($links, $settings_link); 
+    return $links; 
+}
---
+++ readme.txt
@@ -0,0 +1,81 @@
+=== Plugin Name ===
+Contributors: osdwebdev
+Tags: outdated, old, browser
+Requires at least: 3.4
+Tested up to: 4.0
+Stable tag: 1.5
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+OSD Outdated Browser widget allows you to display a custom banner for users using old browsers.
+
+== Description ==
+
+OSD Outdated Browser widget allows you to display a custom banner for users using old browsers.  
+The banner is fully customizable and also exposes filters for developers to use. All a user needs to do
+is install/activate the plugin, set up the proper settings, then add the OSD Outdated Browser widget 
+to a widget area.
+
+== Installation ==
+
+1. Upload the osd-outdated-browser directory to your `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Visit the settings page under Settings > OSD Outdated Browser
+4. Add the OSD Outdated Browser widget to any widget area in your theme.
+5. If the default message is not used, be sure to include an element with the id "osd-outdated-browser-close" in order to close the banner and set a cookie based on user preferences.
+6. Styles can be easily overridden in the theme's style sheet (style.css) or in the WYSIWYG editor
+7. If you desire more control over how the banner is displayed, a WordPress filter is available. just use the following code: add_filter('osd-outdated-browser', function($content, $args) {}, 10, 2);
+8. Additionally, the WordPress filters content_edit_pre and content_save_pre are applied to the banner content.
+
+== Frequently Asked Questions ==
+
+= Soon to come? =
+
+Yes, as users ask us questions.
+
+== Screenshots ==
+
+1. The default banner of OSD Outdated Browser
+2. A slightly themed banner
+3. Admin Settings Screen
+
+== Changelog ==
+
+= 1.5 =
+* Added settings link to plugins page
+
+= 1.4 =
+* Fixed minor styling bug
+* Enabled use with a shortcode
+
+= 1.3 =
+* Fixed a bug where older versions of PHP would not work.
+* Fixed a minor style issue
+* Updated instructions because they were terrible and mentioned nothing of being a widget
+
+= 1.2 =
+* Fixed a style bug in IE.
+
+= 1.1 =
+* Fixed a logic bug where javascripts and styles were being inserted regardless of the banner showing or not.
+
+= 1.0 =
+* Initial creation of the OSD Outdated Browser
+
+== Upgrade Notice ==
+
+= 1.0 =
+Display a banner in outdated browsers notifying user to update
+
+== A Brief Feature List ==
+
+1. Fully customizable
+2. Filterable banner message using add_filter() for theme developers
+3. Easy to style
+4. Minimal Code
+5. No added load on your installation
+
+Link to plugin page [Wordpress plugin page](http://wordpress.org/plugins/osd-outdated-browser/ "Link").
+
+[markdown syntax]: http://daringfireball.net/projects/markdown/syntax
+            "Markdown is what the parser uses to process much of the readme file"
